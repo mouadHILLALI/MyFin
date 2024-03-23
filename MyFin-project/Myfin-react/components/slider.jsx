6d5108950df971ce278e48853a696a88298fd5b4
mouadHILLALI <mauadtoto001@gmail.com>
2024-03-23 13:57:34 +0000

    adding slider

---
+++ MyFin-project/Myfin-react/components/slider.jsx
@@ -0,0 +1,17 @@
+
+function Slider(){
+return <>
+    <section className="w-[80%] m-auto mt-3">
+        <div>
+            <h1 className="font-bold text-5xl">We Encourage Philanthropy </h1>
+            <button></button>
+            <button></button>
+        </div>
+        <div></div>
+        <div></div>
+    </section>
+</>
+
+}
+
+export default Slider;
--- MyFin-project/Myfin-react/pages/home.jsx
+++ MyFin-project/Myfin-react/pages/home.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
-
+import Slider from "../components/slider";
 function Home(){
     return <>
     <Navbar/>
     <Hero/>
+    <Slider/>
     </>
 }
 export default Home ;
