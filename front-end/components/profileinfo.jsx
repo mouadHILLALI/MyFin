efae215ebdd508a343c9c65355a4bd99f17d93c4
mouadHILLALI <mauadtoto001@gmail.com>
2024-04-11 12:43:56 +0000

    debugging symlink not showing images

--- front-end/components/profileinfo.jsx
+++ front-end/components/profileinfo.jsx
@@ -1,40 +1,54 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 export const ProfileInfo = () => {
   const [user, setUser] = useState({});
-  let token = localStorage.getItem('token');
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const token = localStorage.getItem("token");
         const res = await axios.get("http://localhost/api/user/data", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-        let userData = res.data.user;
-        let accountData = res.data.account;
-        let updatedUser = {
+        const userData = res.data.user;
+        const accountData = res.data.account;
+        const updatedUser = {
           owner: userData,
           info: accountData,
         };
         setUser(updatedUser);
+        setIsLoading(false);
+        console.log(user);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setIsLoading(false);
       }
     };
 
     fetchData();
-  }, []); 
-  console.log(user);
+  }, []);
+
   return (
     <div>
-      <div className=" fixed z--1 p-4 bg-[#02A95C] w-full h-[20vh] rounded-b-[25px]  ">
-        <div className="h-[5%]"></div>
-        <div className="fixed top-[20%] left-[4%] rounded-[50%] bg-black  w-[15%] h-[30%] flex">
-          <h1 className="fixed top-[25%] left-[20%]">{name}</h1>
-          <h1 className="fixed top-[35%] left-[20%]">ef</h1>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className=" fixed z--1 p-4 bg-[#02A95C] w-full h-[20vh] rounded-b-[25px]  ">
+          <div className="h-[5%]"></div>
+          <div className="fixed top-[20%] left-[4%] rounded-[50%] bg-black  w-[15%] h-[30%] flex">
+            <span className="fixed gap-2 flex top-[25%] left-[20%]">
+              <h1>{user.owner.first_name}</h1>
+              <h1>{user.owner.family_name}</h1>
+            </span>
+            <h1 className="fixed top-[35%] left-[20%]">{user.owner.role}</h1>
+          </div>
+          <img src={user.owner.image} width={30} alt="..." />
         </div>
-      </div>
+      )}
     </div>
   );
 };
