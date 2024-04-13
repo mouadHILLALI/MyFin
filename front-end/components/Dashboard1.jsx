c0d6a0bae027755473cd3ba6b199fdf095600a51
mouadHILLALI <mauadtoto001@gmail.com>
2024-04-13 15:17:52 +0000

    Loan UI

--- front-end/components/Dashboard1.jsx
+++ front-end/components/Dashboard1.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 export const Dashboardinv = () => {
+  let style = "";
   const [slide, setSlide] = useState(false);
-    if(slide){
-        
-    }else{}
+  if (slide) {
+    style =
+      "ease-in-out overflow-auto duration-300 fixed top-[7.5%] w-[30%] h-full bg-[#02A95C] border-2 border-black rounded-r-[14px] ";
+  } else {
+    style =
+      "ease-in-out overflow-auto duration-300 fixed left-[-100%] h-full bg-[#02A95C] border-2 border-black rounded-r-[14px] top-[7.5%] w-[30%] h-[80%]";
+  }
   return (
     <>
       <div className=" w-[90%] m-auto flex gap-7 ">
@@ -42,13 +47,75 @@ export const Dashboardinv = () => {
         <h1>You have no Loans yet</h1>
       </div>
 
-      <div className=" fixed top-[7.5%] w-[30%] h-[80%] ">
-        <form>
-          <input type="text"  placeholder="imhere"/>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+      <div className={style}>
+        <div className=" flex justify-end p-4">
+          <button className="mr-4" onClick={() => setSlide(false)}>
+            <svg
+              width={30}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path
+                fill="#ffffff"
+                d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"
+              />
+            </svg>
+          </button>
+        </div>
+        <form className="flex flex-col h-full gap-4 ">
+          <div className="flex">
+            <input
+              className="p-2 rounded-[15px] w-[80%] m-auto"
+              min={1000}
+              type="number"
+              placeholder="Enter the Amount of the Loan :"
+            />
+          </div>
+          <label className="text-white text-lg m-4 " htmlFor="duration">
+            Enter the loan Riembursement Duration:
+          </label>
+          <div id="duration" className="flex justify-around m-4 ">
+            <input
+              className=" w-[30%] p-2 rounded-lg "
+              min={0}
+              type="number"
+              placeholder="Years"
+            />
+            <input
+              className=" w-[30%] p-2 rounded-lg "
+              min={0}
+              max={12}
+              type="number"
+              placeholder="Months"
+            />
+            <input
+              className=" w-[30%] p-2 rounded-lg "
+              min={10}
+              max={31}
+              type="number"
+              placeholder="Days"
+            />
+          </div>
+          <div className="flex">
+            <input
+              className="w-[80%] m-auto p-2 rounded-[15px] "
+              type="number"
+              min={1}
+              placeholder="Enter the Loan profit Rate :"
+            />
+          </div>
+          <div className="flex flex-col ">
+            <div className="flex flex-col items-center gap-3 m-auto">
+              <label className="text-white text-lg ">Upload Your Bussiness Model:</label>
+            <label htmlFor="file">
+              <svg width={80} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path fill="#ffffff" d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z" />
+              </svg>
+            </label>
+            <input id="file" type="file" className="hidden" />
+            </div>
+          </div>
+          <button className=" p-2 bg-white text-white text-lg" type="submit">Submit for Review</button>
         </form>
       </div>
     </>
